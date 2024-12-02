e7666490b60eb7ff02f3800afac0a0bb499b5bd4
toomyem <toomyem@gmail.com>
2024-12-02 11:20:08 +0100

    Day 1

--- day01/day01.ml
+++ day01/day01.ml
@@ -5,14 +5,14 @@ let count el list =
 ;;
 
 let () =
-  let data =
+  let list1, list2 =
     Tools.read_lines ()
     |> List.map ~f:Tools.to_int_list
     |> List.map ~f:(fun lst -> List.nth_exn lst 0, List.nth_exn lst 1)
     |> List.unzip
   in
-  let sorted1 = List.sort (fst data) ~compare:Int.compare in
-  let sorted2 = List.sort (snd data) ~compare:Int.compare in
+  let sorted1 = List.sort list1 ~compare:Int.compare in
+  let sorted2 = List.sort list2 ~compare:Int.compare in
   let data = List.map2_exn ~f:(fun a b -> abs (a - b)) sorted1 sorted2 in
   let n1 = List.fold ~init:0 ~f:(fun acc a -> acc + a) data in
   let n2 = List.fold ~init:0 ~f:(fun acc a -> acc + (a * count a sorted2)) sorted1 in
