1fe7ba72963a352c305975a3e5487ffb0c097e36
toomyem <toomyem@gmail.com>
2024-12-12 16:59:06 +0100

    Day 12

--- day12/main.ml
+++ day12/main.ml
@@ -1,43 +1,57 @@
 open Base
 
-let coords board =
-  let h = Array.length board
-  and w = Array.length board.(0) in
+type direction =
+  | Up
+  | Down
+  | Left
+  | Right
+
+let dirs = [ Up; Down; Left; Right ]
+
+let move pos dir =
+  let r, c = pos in
+  match dir with
+  | Up -> r - 1, c
+  | Down -> r + 1, c
+  | Left -> r, c - 1
+  | Right -> r, c + 1
+;;
+
+let board_dim b = Array.length b, Array.length b.(0)
+
+let coords b =
+  let h, w = board_dim b in
   List.cartesian_product (List.range 0 h) (List.range 0 w)
 ;;
 
 let get_at board pos =
-  let r = fst pos
-  and c = snd pos in
-  let h = Array.length board
-  and w = Array.length board.(0) in
+  let r, c = pos in
+  let h, w = board_dim board in
   if r >= 0 && r < h && c >= 0 && c < w then board.(r).(c) else '.'
 ;;
 
-let visit board pos =
-  let r = fst pos
-  and c = snd pos in
-  let ch = board.(r).(c) in
-  board.(r).(c) <- Char.lowercase ch
+let set_at board pos ch =
+  let r, c = pos in
+  let h, w = board_dim board in
+  if r >= 0 && r < h && c >= 0 && c < w then board.(r).(c) <- ch
 ;;
 
-let rec fill board pos =
-  let ch = get_at board pos in
-  if Char.equal ch '.'
-  then []
-  else (
-    visit board pos;
-    let r = fst pos
-    and c = snd pos in
-    let w =
-      List.map
-        ~f:(fun (dy, dx) ->
-          let npos = r + dy, c + dx in
+let fill board pos =
+  let rec fill0 board pos =
+    let ch = get_at board pos in
+    if Char.equal ch '.'
+    then []
+    else (
+      set_at board pos (Char.lowercase ch);
+      let w =
+        List.map dirs ~f:(fun dir ->
+          let npos = move pos dir in
           let nch = get_at board npos in
-          if Char.equal ch nch then fill board npos else [])
-        [ 0, 1; 1, 0; -1, 0; 0, -1 ]
-    in
-    pos :: List.concat w)
+          if Char.equal ch nch then fill0 board npos else [])
+      in
+      pos :: List.concat w)
+  in
+  fill0 board pos
 ;;
 
 let fence board area =
@@ -45,15 +59,11 @@ let fence board area =
     List.map
       ~f:(fun pos ->
         let ch = get_at board pos in
-        let r = fst pos
-        and c = snd pos in
         let f =
-          List.map
-            ~f:(fun (dy, dx) ->
-              let npos = r + dy, c + dx in
-              let nch = get_at board npos in
-              if Char.equal ch nch then 1 else 0)
-            [ 0, 1; 1, 0; -1, 0; 0, -1 ]
+          List.map dirs ~f:(fun dir ->
+            let npos = move pos dir in
+            let nch = get_at board npos in
+            if Char.equal ch nch then 1 else 0)
         in
         4 - Tools.sum_of_ints f)
       area
@@ -66,14 +76,14 @@ let calc_cost board pos =
   if Char.between ~low:'A' ~high:'Z' ch
   then (
     let area = fill board pos in
-    Stdlib.Printf.printf "%d\n" (List.length area);
     List.length area * fence board area)
   else 0
 ;;
 
+let read_board = Tools.read_lines () |> List.map ~f:String.to_array |> List.to_array
+
 let () =
-  let lines = Tools.read_lines () in
-  let board = List.to_array lines |> Array.map ~f:String.to_array in
+  let board = read_board in
   let costs = List.map ~f:(calc_cost board) (coords board) in
   let n = Tools.sum_of_ints costs in
   Stdlib.Printf.printf "Solution 1: %d\n" n
