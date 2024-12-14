8827c8cfe1412a00e41a3bc14041637bb5f4e828
toomyem <toomyem@gmail.com>
2024-12-14 19:49:14 +0100

    Day 12

--- day12/main.ml
+++ day12/main.ml
@@ -1,4 +1,5 @@
 open Base
+open Tools
 
 type direction =
   | Up
@@ -17,23 +18,9 @@ let move pos dir =
   | Right -> r, c + 1
 ;;
 
-let board_dim b = Array.length b, Array.length b.(0)
-
 let coords b =
-  let h, w = board_dim b in
+  let w, h = board_dim b in
   List.cartesian_product (List.range 0 h) (List.range 0 w)
-;;
-
-let get_at board pos =
-  let r, c = pos in
-  let h, w = board_dim board in
-  if r >= 0 && r < h && c >= 0 && c < w then board.(r).(c) else '.'
-;;
-
-let set_at board pos ch =
-  let r, c = pos in
-  let h, w = board_dim board in
-  if r >= 0 && r < h && c >= 0 && c < w then board.(r).(c) <- ch
 ;;
 
 let rec fill board pos =
@@ -41,7 +28,7 @@ let rec fill board pos =
   if not (Char.between ~low:'A' ~high:'Z' ch)
   then []
   else (
-    set_at board pos (Char.lowercase ch);
+    update_at board pos ~f:Char.lowercase;
     let w =
       List.map dirs ~f:(fun dir ->
         let npos = move pos dir in
@@ -62,10 +49,10 @@ let fence1 board area =
             let nch = get_at board npos in
             if Char.equal ch nch then 1 else 0)
         in
-        4 - Tools.sum_of_ints f)
+        4 - sum_of_ints f)
       area
   in
-  List.length area * Tools.sum_of_ints costs
+  List.length area * sum_of_ints costs
 ;;
 
 let in_area area pos =
@@ -79,10 +66,10 @@ let inc r = r := !r + 1
 let fence2 board area =
   let h, w = board_dim board in
   let s = ref 0 in
-  List.iter (List.range 0 h) ~f:(fun r ->
+  for r = 0 to h do
     let count_up = ref true in
     let count_down = ref true in
-    List.iter (List.range 0 w) ~f:(fun c ->
+    for c = 0 to w do
       let pos = r, c in
       if in_area area pos
       then (
@@ -105,11 +92,13 @@ let fence2 board area =
         else if !count_down
         then (
           inc s;
-          count_down := false))));
-  List.iter (List.range 0 w) ~f:(fun c ->
+          count_down := false))
+    done
+  done;
+  for c = 0 to w do
     let count_left = ref true in
     let count_right = ref true in
-    List.iter (List.range 0 h) ~f:(fun r ->
+    for r = 0 to h do
       let pos = r, c in
       if in_area area pos
       then (
@@ -132,20 +121,20 @@ let fence2 board area =
         else if !count_right
         then (
           inc s;
-          count_right := false))));
+          count_right := false))
+    done
+  done;
   !s * List.length area
 ;;
 
-let read_board = Tools.read_lines () |> List.map ~f:String.to_array |> List.to_array
-
 let () =
-  let board = read_board in
+  let board = read_board () in
   let areas =
     List.map ~f:(fill board) (coords board)
     |> List.filter ~f:(fun a -> not (List.is_empty a))
   in
-  let n1 = List.map ~f:(fence1 board) areas |> Tools.sum_of_ints in
-  let n2 = List.map ~f:(fence2 board) areas |> Tools.sum_of_ints in
+  let n1 = List.map ~f:(fence1 board) areas |> sum_of_ints in
+  let n2 = List.map ~f:(fence2 board) areas |> sum_of_ints in
   Stdlib.Printf.printf "Solution 1: %d\n" n1;
   Stdlib.Printf.printf "Solution 2: %d\n" n2
 ;;
