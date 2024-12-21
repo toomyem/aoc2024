3c70d64fe996d9350db5778e27c55911cd7a6b8c
toomyem <toomyem@gmail.com>
2024-12-21 19:41:50 +0100

    Validation

--- validate.sh
+++ validate.sh
@@ -4,6 +4,9 @@ set -euo pipefail
 
 for d in day*
 do
-  echo "Solving $d"
- ( cd "$d" && ./run.sh )
+  echo "--> Solving $d"
+  [[ "$d" = "day16" ]] && echo "Skip validation until performance is fixed" && continue
+  SECONDS=0
+  ( cd "$d" && ./run.sh )
+  echo "Solved in $SECONDS secs"
 done
