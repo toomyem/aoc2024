03ddb5adf1cf015baa7c5cfb7f3f0979b468ba96
toomyem <toomyem@gmail.com>
2024-12-25 22:54:20 +0100

    Day 21 wip

--- day21/main.ml
+++ day21/main.ml
@@ -30,31 +30,9 @@ let paths pad (r1, c1) (r2, c2) =
   else if dc = 0
   then [ move_v dr ]
   else (
-    let p = if equal_char pad.(r2).(c1) ' ' then [] else [ move_v dr ^ move_h dc ] in
-    let p = if equal_char pad.(r1).(c2) ' ' then p else (move_h dc ^ move_v dr) :: p in
+    let p = if equal_char pad.(r1).(c2) ' ' then [] else [ move_h dc ^ move_v dr ] in
+    let p = if equal_char pad.(r2).(c1) ' ' then p else (move_v dr ^ move_h dc) :: p in
     p)
-;;
-
-(* let pos_to_str pos = "(" ^ Int.to_string (fst pos) ^  "," ^ Int.to_string (snd pos) ^")" *)
-(* let p = paths keypad (3, 2) (1, 0) in
-  List.iter p ~f:(fun path ->
-    Stdlib.Printf.printf
-      "%s\n"
-      (List.fold path ~init:"" ~f:(fun acc v -> acc ^ Char.to_string v))); *)
-
-let find_paths pad keys =
-  let found = ref [] in
-  let pos = find pad 'A' in
-  let rec moves pos i acc =
-    if i = String.length keys
-    then found := acc :: !found
-    else (
-      let pos2 = find pad (String.get keys i) in
-      paths pad pos pos2
-      |> List.iter ~f:(fun path -> moves pos2 (i + 1) (acc ^ path ^ "A")))
-  in
-  moves pos 0 "";
-  !found
 ;;
 
 let shortest pp =
@@ -66,39 +44,50 @@ let enc pad k p =
   let a = find pad p in
   let b = find pad k in
   let pp = paths pad a b in
-  let sp = shortest pp in
-  (* Stdlib.Printf.printf "enc: %c to %c = %s\n" p k (sp ^ "A"); *)
-  sp ^ "A"
+  List.map pp ~f:(fun s -> s ^ "A")
 ;;
+
+(* List.map pp ~f:(fun p -> p ^ "A") *)
 
 (* let last_char s = String.to_list_rev s |> List.hd_exn *)
 
+let h = Hashtbl.create (module String)
+
 let rec encode (keys : string) lvl m : string =
-  (* Stdlib.Printf.printf "encode: %s (%d)\n" keys lvl; *)
-  if lvl = m
-  then keys
-  else (
-    let pad = if lvl = 0 then keypad else dirpad in
-    List.fold (String.to_list keys) ~init:("", 'A') ~f:(fun (acc, p) k ->
-      let e = enc pad k p in
-      let ee = encode e (lvl + 1) m in
-      acc ^ ee, k)
-    |> fst)
+  Stdlib.Printf.printf "encode: %s (lvl=%d)\n" keys lvl;
+  Stdlib.flush Stdio.stdout;
+  match Hashtbl.find h (keys ^ Int.to_string lvl) with
+  | Some x -> x
+  | None ->
+    let x =
+      if lvl = m
+      then keys
+      else (
+        let pad = if lvl = 0 then keypad else dirpad in
+        String.fold keys ~init:("", 'A') ~f:(fun (acc, p) k ->
+          let e = enc pad k p in
+          let ee = List.map e ~f:(fun e -> encode e (lvl + 1) m) |> shortest in
+          acc ^ ee, k)
+        |> fst)
+    in
+    Hashtbl.set h ~key:(keys ^ Int.to_string lvl) ~data:x;
+    x
 ;;
 
 let () =
   let codes = Tools.read_lines () in
   let n =
     List.fold codes ~init:0 ~f:(fun acc code ->
-      let pp = encode code 0 3 in
+      let p = encode code 0 21 in
       let v = Int.of_string (String.chop_suffix_if_exists ~suffix:"A" code) in
       Stdlib.Printf.printf
-        "%s to %s len=%d %d\n"
+        "%s %s len=%dMB %d\n"
         code
-        pp
-        (String.length pp)
-        (v * String.length pp);
-      acc + (v * String.length pp))
+        (String.prefix p 80)
+        (String.length p / 1024 / 1024)
+        (v * String.length p);
+      Stdlib.flush Stdio.stdout;
+      acc + (v * String.length p))
   in
   Stdlib.Printf.printf "Solution 1: %d\n" n
 ;;
